fd23b46b648832b860517bf1dbbabebdd35022d8
Rohinimelam <rohinimelam123@gmail.com>
2022-12-08 10:33:10 +0530

    final

--- src/test/java/com/plantshop/PlantShopBackendApplicationTests.java
+++ src/test/java/com/plantshop/PlantShopBackendApplicationTests.java
@@ -1,9 +1,11 @@
 package com.plantshop;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration
+
 class PlantShopBackendApplicationTests {
 
 	@Test
